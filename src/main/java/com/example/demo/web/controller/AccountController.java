c47a26ea46878bb2ef80dca0b6652bd11c68e1a4
张平平 <zhangpingping1981@163.com>
2025-04-23 08:02:44 +0800

    new

---
+++ src/main/java/com/example/demo/web/controller/AccountController.java
@@ -0,0 +1,39 @@
+package com.example.demo.web.controller;
+
+import com.example.demo.application.service.AccountService;
+import com.example.demo.domain.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/create")
+    public ResponseEntity createAccount(@RequestBody Account account) {
+
+
+        String result =  accountService.createAccount(account);
+        if(result == null)
+        {
+            return ResponseEntity.ok("Account created successfully");
+        }
+        else {
+            return ResponseEntity.badRequest().body(result);
+        }
+
+
+
+    }
+
+    @PutMapping("/changestatus")
+    public void changeAccountStatus(@RequestBody Account account) {
+        accountService.changeAccountStatus(account);
+    }
+
+
+}
