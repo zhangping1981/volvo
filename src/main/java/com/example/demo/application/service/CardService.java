096b83d5934f79bc62c1c70852850d8631229c1b
张平平 <zhangpingping1981@163.com>
2025-04-23 11:02:27 +0800

    new

--- src/main/java/com/example/demo/application/service/CardService.java
+++ src/main/java/com/example/demo/application/service/CardService.java
@@ -4,8 +4,10 @@ import com.example.demo.domain.model.Card;
 import com.example.demo.domain.model.CardStatus;
 
 public interface CardService {
-    Card createCard(Card card);
+    String  createCard(Card card);
     void assignCardToAccount(Long cardId, Long accountId);
     void changeCardStatus(Long cardId, CardStatus status);
+
+
 
 }
